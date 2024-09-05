f247d86144289346acbff96ba94cfed2699f55b9
LinconUNIPAR <lincon.schmitt@edu.unipar.br>
2024-09-05 07:38:19 -0300

    att

---
+++ src/Atividade2.java
@@ -0,0 +1,13 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+
+        int [] numeros = new int [4];
+
+        numeros[0] = 3;
+        numeros[1] = 6;
+        numeros[2] = 9;
+        numeros[3] = 12;
+
+        System.out.println("a soma do primeiro e do ultimo número é : " + numeros[0] + numeros[3]);
+    }
+}
