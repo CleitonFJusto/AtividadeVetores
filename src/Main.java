bc6d231293f117d5e86b7295e68cb80bd34d964a
LinconUNIPAR <lincon.schmitt@edu.unipar.br>
2024-09-05 07:04:05 -0300

    exemplovetores

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int votoPessoa1 = 153420;
+        int votoPessoa2 = 10;
+        int votoPessoa3 = 1510;
+        int votoPessoa4 = 10;
+        int votoPessoa5 = 10;
+        int votoPessoa6 = 10;
+        int votoPessoa7 = 1540;
+        int votoPessoa8 = 1460;
+        int votoPessoa9 = 105;
+        int votoPessoa10 = 1650;
+        int votoPessoa11 = 10;
+        int votoPessoa12 = 3760;
+
+        //primera forma de criar e inicilizar um vetor
+        //criamos o vetor vazio
+        int [] votos = new int[10];
+
+        //nomeVetor [posicao] = valor
+        votos[0] = 90;
+        votos[1] = 80;
+        votos[2] = 70;
+        votos[3] = 60;
+        votos[4] = 50;
+        votos[5] = 40;
+        votos[6] = 30;
+        votos[7] = 20;
+        votos[8] = 10;
+       votos[9] = 154;
+       String[] nomes = new String[10];
+
+    Scanner entrada = new Scanner(System.in);
+    nomes[0] = entrada.nextLine();
+
+    System.out.println("O primeiro nome é : " + nomes[0]);
+
+   }
+}
